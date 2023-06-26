f5eebef0561550f642535b604285e46a863accac
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-06-26 07:05:05 +0530

    add test url

--- app/core/app.php
+++ app/core/app.php
@@ -1,8 +1,19 @@
 <?php 
 class App{
 
+    protected $controller = "home";
+    protected $method = "index";
+    protected $prans = [];
+
     public function __construct(){
         echo "OK!";
+        $this->praseUrl();
+    }
+
+    public function praseUrl(){
+        if(isset($_GET['url'])){
+            echo $_GET['url'];
+        }
     }
 }
 
